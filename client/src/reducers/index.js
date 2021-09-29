88b43533fe78b40dadb52b483599a4a2d3854b8c
mabnf93 <mabnf93@gmail.com>
2021-09-29 18:00:37 +0100

    clean

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import posts from './posts'
-import loadingValue from './loadingValue'
-import auth from './auth'
-export default combineReducers({
-    loadingValue,
-    posts,
-    auth,
-})
