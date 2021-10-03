a1804d78fcab3eee4793384ac1a556132dd619b3
mabnf93 <mabnf93@gmail.com>
2021-10-03 21:47:15 +0100

    cleaning

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: "https://mern-ishare-app.herokuapp.com/" });
+const API = axios.create({ baseURL: "http://localhost:3000" });
 
 
 API.interceptors.request.use((req) => {
