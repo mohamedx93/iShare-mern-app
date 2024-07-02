a04c414185fe6e2fd7e1cd0c3b35cbca3d24479f
mohamedx93 <mabnf93@gmail.com>
2024-07-02 13:32:23 +0100

    Update index.js
    update the API URL (migrating the backend from heroku to render)

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: "https://mern-ishare-app.herokuapp.com/" });
+const API = axios.create({ baseURL: "https://ishare-mern-app.onrender.com/" });
 
 
 API.interceptors.request.use((req) => {
